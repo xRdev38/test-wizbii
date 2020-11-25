d51708c66dd8ec4feb6cd48b2c38500e89b1ff00
developer rudy <developer.rudy@gmail.com>
2020-11-25 23:21:45 +0100

    core: update index import

--- src/app/core/models/index.ts
+++ src/app/core/models/index.ts
@@ -2,4 +2,5 @@ export * from './article.model';
 export * from './comment.model';
 export * from './errors.model';
 export * from './profile.model';
+export * from './tag.model';
 export * from './user.model';
