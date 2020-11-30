22649502ce8a1c1b18ebd0be502b68db4ea34ee5
developer rudy <developer.rudy@gmail.com>
2020-11-30 23:57:56 +0100

    core: add article module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,8 @@ import { NgModule } from '@angular/core';
 import { CoreModule } from '@app/core';
 import { SharedModule } from "@app/shared";
 import { CommentModule } from "@app/comment/comment.module";
-import { ProfileModule } from "@app//profile/profile.module";
+import { ProfileModule } from "@app/profile/profile.module";
+import { ArticleModule } from "@app/article/article.module";
 import { AppComponent } from './app.component';
 
 @NgModule({
