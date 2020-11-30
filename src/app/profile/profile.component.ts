8995fdd2f17f83786917e8f04547e6ebc1552b1d
developer rudy <developer.rudy@gmail.com>
2020-11-30 23:40:27 +0100

    feature: create profile component

---
+++ src/app/profile/profile.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from "@angular/core";
+
+import {
+  IUser,
+} from '@app/core';
+
+@Component({
+  selector: "app-profile",
+  templateUrl: "./profile.component.html",
+  styleUrls: ["./profile.component.scss"]
+})
+export class ProfileComponent {
+  @Input() user: IUser | null = null;
+}
---
+++ src/app/profile/profile.module.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+
+import { ProfileComponent } from "./profile.component";
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [ProfileComponent],
+  exports: [ProfileComponent]
+})
+export class ProfileModule {}
