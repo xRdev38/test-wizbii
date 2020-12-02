71d6c6d5a3f89101e51400fab10f26bd7e82a4b5
developer rudy <developer.rudy@gmail.com>
2020-12-02 05:22:16 +0100

    refactor: move logic

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,16 +1,75 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
-import { ProfilesService } from '@app/core';
-import { IArticleView } from '@app/article/article-view.interface';
+import {
+  ITag,
+  IArticle,
+  IComment,
+  TagsService,
+  CommentsService,
+  ProfilesService
+} from '@app/core';
+
+import { IProfileView } from '@app/shared/article-helpers/profile-view.interface';
 
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
   styleUrls: ['./article.component.scss']
 })
-export class ArticleComponent {
-  @Input() dataView: IArticleView | null = null;
+export class ArticleComponent implements OnInit {
+  @Input() article: IArticle | null = null;
+
+
+  tags$: Observable<ITag[]> | null = null;
+  comments$: Observable<IComment[]> | null = null;
+  author$: Observable<IProfileView> | null = null;
+
+  constructor(
+    private tagsService: TagsService,
+    private profilesService: ProfilesService,
+    private commentsService: CommentsService
+  ){}
+
+  ngOnInit(): void {
+      this.createTags();
+      this.createAuthor();
+      this.createComments();
+  }
+
+  createTags(): void {
+    if( this.article !== null ) {
+      this.tags$ = this.tagsService.getAll()
+        .pipe(
+            map( tag => {
+                return this.article !== null ? this.tagsService.filterTags(tag, this.article.tagList) : [];
+            })
+        )
+    }
+  }
+
+  createAuthor(): void {
+    if( this.article !== null ) {
+      this.author$ = this.profilesService.getById(this.article.profileId)
+        .pipe( map( profile => {
+            return {
+              ...profile,
+              createdAt: this.article?.createdAt ? this.article.createdAt : ''
+            };
+          })
+        )
+    }
+  }
+
+  createComments(): void {
+    if( this.article !== null ) {
+      this.comments$ = this.commentsService.getAll().pipe(
+        map( comments => {
+          return this.article !== null ? this.commentsService.filterComments(comments, this.article.id) : [];
+        })
+      );
+    }
+  }
 }
